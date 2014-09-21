185b7e1981318fd81f5f07a02b006b697b2bfa09
yoshiky <y.nagatsuka0524@gmail.com>
2014-09-22 00:34:28 +0900

    Add cucumber and utilities.rb

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,7 +14,8 @@ describe "Authentications" do
       before { click_button 'Sign in' }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      #it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
         before { click_link 'Home' }
@@ -24,11 +25,12 @@ describe "Authentications" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+     #before do
+     #  fill_in "Email", with: user.email.upcase
+     #  fill_in "Password", with: user.password
+     #  click_button "Sign in"
+     #end
+      before { valid_signin(user) }
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
