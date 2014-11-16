0b27bcc8871e56da4e01745d475be1cfdcf2fb90
yoshiky <y.nagatsuka0524@gmail.com>
2014-11-16 10:33:18 +0900

    Add user following

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -20,7 +20,7 @@ def sign_in(user, options={})
   end
 end
 
-Rspec::Matchers.define :have_error_message do |message|
+RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
   end
